4e9c922ecb9f96c4a8f60b2e91d7949e0b967dfc
Sahasraallari <mohan.velagala@gmail.com>
2025-01-24 17:48:34 +0530

    operator

---
+++ operator.py
@@ -0,0 +1,25 @@
+print("My name is Sahasra")
+print("Iam styuding in grade5")
+print("My school name is AHPS")
+print("Ilike yellow color")
+print("I like icecream")
+
+a=78
+b=56
+
+print("addition:", a+b)
+print("subtraction:", a-b)
+print("multiplication:", a*b)
+print("division:", a/b)
+
+a=34
+print("data type of a is",type(a))
+
+v=1.14
+print("data type of a is",type(v))
+
+t='False'
+print("data type of a is",type(t))
+
+g="sahasra"
+print("data type of a is",type(g))
