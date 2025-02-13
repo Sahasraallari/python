88629998d46033ec3fffd6c344a87af30b1e8ece
Sahasraallari <mohan.velagala@gmail.com>
2025-02-13 17:42:27 +0530

    functions

---
+++ function .py
@@ -0,0 +1,16 @@
+def introduction(a):
+    print("My name is ",a)
+name=input("Enter your name:")
+introduction(name)
+
+def Day (a):
+    print("Good Morning ",a)
+name=input("Enter your name:")
+Day(name)
+
+def absolute(num):
+    if num>=0:
+          return num
+    else:
+          return -num
+print(absolute(-45))
