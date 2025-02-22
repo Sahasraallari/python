f86e796743120909111526db90933fdd5cba5143
Sahasraallari <mohan.velagala@gmail.com>
2025-02-22 17:00:52 +0530

    dictonaries

--- rectangle.py
+++ rectangle.py
@@ -1,8 +1,20 @@
 class rectangle:
     def __init__(self,l,w):
-      self.length= l
-      self.width = w
+      self.l= l
+      self.w = w
     def area(self):
         return self.l*self.w
 obj = rectangle(23,34)
 print(obj.area())
+
+class rectangle:
+    def __init__(self,l,w):
+      self.l= l
+      self.w=w
+    def area(self):
+        return 2*(self.l+self.w)
+obj = rectangle(23,34)
+print(obj.area())
+
+
+
