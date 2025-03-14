fc232005bfb3e37483e21c80e95f4506b1fcee1f
Sahasraallari <mohan.velagala@gmail.com>
2025-03-14 17:25:17 +0530

    address book

--- address book.py
+++ address book.py
@@ -12,12 +12,31 @@ def search__contact(name):
       if name in address__book:
             print(f"{name}:{address__book[name]}")
       else:
-            print("contact not found.")
+            print("contact not found.") 
+while True:  
+      print("\n1. Add Contact\n2. View Contacts\n3. Search Contact\n4. Exit")
+      choice = input("Enter choice: ")
+      if choice == "1":   
+            name = input("Enter name: ")
+            phone = input("Enter phone: ")
+            email = input("Enter the choice:") 
+            add__contact(name,phone,email) 
+      elif choice == "2":
+           view__contact()
+      elif choice == "3":
+           name=("Enter name to search:")
+           search__contact  (name)
+      elif choice == "4":
+           print("Exiting address book.")
+           break
+      else:
+           print("Invalid choice,try again.")
+           
 
-
-
-
-
-         
       
+      
+
+
+
+
 
